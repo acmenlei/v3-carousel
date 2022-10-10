9927e18e77cde249eb763ea1af1fcb3bf0e4379c
coderlei <coderleilei@163.com>
2022-10-10 17:13:33 +0800

    fix: online demo

--- examples/demo/src/main.ts
+++ examples/demo/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import V3Carousel from "../../../"
+import V3Carousel from "v3-carousel"
 
 createApp(App).use(V3Carousel).mount('#app')
